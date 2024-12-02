08db5822dbf53103922f2b141212b5d98d34763a
Martín Pravia <marto.pravia@gmail.com>
2024-12-02 20:47:52 +0000

    quedan cambios aplicados, no se veia diagram.png por falta de importacion de Enum

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -8,47 +8,43 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "Users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(120), unique=True, nullabase=False)
-    firstname = Column(String(120), nullabase=False)
-    lastname = Column(String(120), nullabase=False)
-    email = Column(String(150), unique=True, nullabase=False)
-
-
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    username = Column(String(120), nullable=False)
+    firstname = Column(String(120), nullable=False)
+    lastname = Column(String(120), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    
 class Comment(Base):
-    __tablename__ = "Comment"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     comment_text = Column(String)
-    author_id = Column(Integer, Foreing_key("User.id"))
-    post_id = Column(Integer, Foreign_key("Post.id"))
-    user = relationship("User", backref="comment")
+    author_id = Column(Integer, ForeignKey("users.id"))
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user = relationship("User", backref="comments")
+    post = relationship("Post", backref = "comments")
 
 class Post(Base):
-    __tablename__ = "Post"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, Foreing_key("User.id"),nullabase=False)
-    user = relationship("User", backref="post")
-    
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", backref="posts")
+
 class Media(Base):
-    __tablename__ = "Media"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    type = Column(Enum, nullabase=False)
-    url = Column(String, nullabase=False)
-    post_id = Column(Integer, Foreing_key("Post.id"),nullabese=False)
-    post = relationship("Post", backref="media")
-    
+    __tablename__ = "medias"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String(150), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    post = relationship("Post", backref="medias")
+
 class Follower(Base):
-    __tablename__ = "Follower"
-    user_from_id = Column(Integer, primary_key=True, nullabase=False)
-    user_to_id = Column(Integer, primary_key=True, Foreing_key("User.id"), nullabase=False)
-    user_from = relationship("User", backref="follower")
-    
-    
-    
-    
-    
-    
-    
+    __tablename__ = "followers"
+    user_form_id = Column(Integer, ForeignKey("users.id"), primary_key=True, nullable=False)
+    user_to_id = Column(Integer,ForeignKey("users.id"), primary_key= True, nullable=False)
+    user_form = relationship("User", backref="following")
+    user_to = relationship("User", backref="followers")
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
