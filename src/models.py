172208db3c7c906b94cbcf0c1daa060fd2cf0bd6
Martín Pravia <marto.pravia@gmail.com>
2024-12-02 19:31:12 +0000

    dejo cargados en cada uno las rutas con Foreing Keys y relationships

--- src/models.py
+++ src/models.py
@@ -20,25 +20,29 @@ class Comment(Base):
     __tablename__ = "Comment"
     id = Column(Integer, primary_key=True, autoincrement=True)
     comment_text = Column(String)
-    author_id = Column(Integer)
-    post_id = Column(Integer)
+    author_id = Column(Integer, Foreing_key("User.id"))
+    post_id = Column(Integer, Foreign_key("Post.id"))
+    user = relationship("User", backref="comment")
 
 class Post(Base):
     __tablename__ = "Post"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, nullabase=False)
+    user_id = Column(Integer, Foreing_key("User.id"),nullabase=False)
+    user = relationship("User", backref="post")
     
 class Media(Base):
     __tablename__ = "Media"
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(Enum, nullabase=False)
     url = Column(String, nullabase=False)
+    post_id = Column(Integer, Foreing_key("Post.id"),nullabese=False)
+    post = relationship("Post", backref="media")
     
 class Follower(Base):
     __tablename__ = "Follower"
     user_from_id = Column(Integer, primary_key=True, nullabase=False)
-    user_to_id = Column(Integer, primary_key=True, nullabase=False)
-
+    user_to_id = Column(Integer, primary_key=True, Foreing_key("User.id"), nullabase=False)
+    user_from = relationship("User", backref="follower")
     
     
     
